4af3a3d73064bfc5dc011de66c132587073cdaa1
Abdurrahim <ebdurrehim5@gmail.com>
2024-12-11 16:45:28 +0100

    add search functionality (search by id, search by word, get all books (paginated))

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { authenticate } = require('../middlewares/auth');
-const { getAllBooks } = require('../controllers/bookController');
+const { getAllBooks, getBookById, searchBooks} = require('../controllers/bookController');
 const router = express.Router();
 
 router.get('/', authenticate, getAllBooks);
+router.get('/search/:id', authenticate, getBookById);
+router.get('/search/', authenticate, searchBooks)
 
 module.exports = router;
 
